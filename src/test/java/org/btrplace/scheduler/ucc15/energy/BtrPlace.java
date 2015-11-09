8d17728b25449435cd21eadaf0571f0cfc6aafa7
Vincent KHERBACHE <vincent.kherbache@inria.fr>
2015-11-09 18:29:24 +0100

    add all experiments: source code + generated json

---
+++ src/test/java/org/btrplace/scheduler/ucc15/energy/BtrPlace.java
@@ -0,0 +1,222 @@
+package org.btrplace.scheduler.ucc15.energy;
+
+import net.minidev.json.JSONObject;
+import org.btrplace.json.JSONConverterException;
+import org.btrplace.json.plan.ReconfigurationPlanConverter;
+import org.btrplace.model.*;
+import org.btrplace.model.constraint.Fence;
+import org.btrplace.model.constraint.MinMTTR;
+import org.btrplace.model.constraint.Offline;
+import org.btrplace.model.constraint.SatConstraint;
+import org.btrplace.model.view.EnergyView;
+import org.btrplace.model.view.ShareableResource;
+import org.btrplace.model.view.network.Network;
+import org.btrplace.model.view.network.Switch;
+import org.btrplace.plan.ReconfigurationPlan;
+import org.btrplace.scheduler.SchedulerException;
+import org.btrplace.scheduler.choco.DefaultChocoScheduler;
+import org.btrplace.scheduler.choco.DefaultParameters;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by vkherbac on 06/11/15.
+ */
+public class BtrPlace {
+
+    String path = new File("").getAbsolutePath() +
+            "/src/test/java/org/btrplace/scheduler/tests/ucc15/energy/";
+
+    @Test
+    public void EnergyTest() throws SchedulerException {
+
+        // Set nb of nodes and vms
+        int nbNodesRack = 24;
+        int nbSrcNodes = nbNodesRack * 2;
+        int nbDstNodes = nbNodesRack * 1;
+        int nbVMs = nbSrcNodes * 2;
+
+        // Set mem + cpu for VMs and Nodes
+        int memVM = 4, cpuVM = 1;
+        int memSrcNode = 16, cpuSrcNode = 4;
+        int memDstNode = 16, cpuDstNode = 4;
+
+        // Set memoryUsed and dirtyRate (for all VMs)
+        int tpl1MemUsed = 2000, tpl1MaxDirtySize = 5, tpl1MaxDirtyDuration = 3;
+        double tpl1DirtyRate = 0; // idle vm
+        int tpl2MemUsed = 4000, tpl2MaxDirtySize = 96, tpl2MaxDirtyDuration = 2;
+        double tpl2DirtyRate = 3; // stress --vm 1000 --bytes 70K
+        int tpl3MemUsed = 2000, tpl3MaxDirtySize = 96, tpl3MaxDirtyDuration = 2;
+        double tpl3DirtyRate = 3; // stress --vm 1000 --bytes 70K
+        int tpl4MemUsed = 4000, tpl4MaxDirtySize = 5, tpl4MaxDirtyDuration = 3;
+        double tpl4DirtyRate = 0; // idle vm
+
+        // New default model
+        Model mo = new DefaultModel();
+        Mapping ma = mo.getMapping();
+
+        // Create online source nodes and offline destination nodes
+        List<Node> srcNodes = new ArrayList<>(), dstNodes = new ArrayList<>();
+        for (int i = 0; i < nbSrcNodes; i++) {
+            srcNodes.add(mo.newNode());
+            ma.addOnlineNode(srcNodes.get(i));
+        }
+        for (int i = 0; i < nbDstNodes; i++) {
+            dstNodes.add(mo.newNode());
+            ma.addOfflineNode(dstNodes.get(i));
+        }
+
+        // Set boot and shutdown time
+        for (Node n : dstNodes) {
+            mo.getAttributes().put(n, "boot", 120); /*~2 minutes to boot*/
+        }
+        for (Node n : srcNodes) {
+            mo.getAttributes().put(n, "shutdown", 17); /*~30 seconds to shutdown*/
+        }
+
+        // Create running VMs on src nodes
+        List<VM> vms = new ArrayList<>();
+        VM v;
+        for (int i = 0; i < nbSrcNodes; i++) {
+            if (i % 2 == 0) {
+                v = mo.newVM();
+                vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl1MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl1DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl1MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl1MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+                v = mo.newVM();
+                vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl2MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl2DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl2MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl2MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+            } else {
+                v = mo.newVM();
+                vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl3MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl3DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl3MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl3MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+                v = mo.newVM();
+                vms.add(v);
+                mo.getAttributes().put(v, "memUsed", tpl4MemUsed);
+                mo.getAttributes().put(v, "dirtyRate", tpl4DirtyRate);
+                mo.getAttributes().put(v, "maxDirtySize", tpl4MaxDirtySize);
+                mo.getAttributes().put(v, "maxDirtyDuration", tpl4MaxDirtyDuration);
+                ma.addRunningVM(v, srcNodes.get(i));
+            }
+        }
+
+        // Add resource views
+        ShareableResource rcMem = new ShareableResource("mem", 0, 0);
+        ShareableResource rcCPU = new ShareableResource("cpu", 0, 0);
+        for (Node n : srcNodes) {
+            rcMem.setCapacity(n, memSrcNode);
+            rcCPU.setCapacity(n, cpuSrcNode);
+        }
+        for (Node n : dstNodes) {
+            rcMem.setCapacity(n, memDstNode);
+            rcCPU.setCapacity(n, cpuDstNode);
+        }
+        for (VM vm : vms) {
+            rcMem.setConsumption(vm, memVM);
+            rcCPU.setConsumption(vm, cpuVM);
+        }
+        mo.attach(rcMem);
+        mo.attach(rcCPU);
+
+        // Add the EnergyView and set nodes & vms consumption
+        int powerIdleNode = 110, powerVM = 16, powerBoot = 20;  // Define power values
+        EnergyView energyView = new EnergyView(Integer.MAX_VALUE);
+        energyView.setBootOverhead(powerBoot); // % energy overhead during boot
+        for (Node n : srcNodes) {
+            energyView.setConsumption(n, powerIdleNode);
+        }
+        for (Node n : dstNodes) {
+            energyView.setConsumption(n, powerIdleNode);
+        }
+        for (VM vm : vms) {
+            energyView.setConsumption(vm, powerVM);
+        }
+        mo.attach(energyView);
+
+        // Add a NetworkView view
+        Network net = new Network();
+        Switch swSrcRack1 = net.newSwitch();
+        Switch swSrcRack2 = net.newSwitch();
+        Switch swDstRack1 = net.newSwitch();
+        Switch swMain = net.newSwitch();
+        net.connect(1000, swSrcRack1, srcNodes.subList(0, nbNodesRack));
+        net.connect(1000, swSrcRack2, srcNodes.subList(nbNodesRack, nbNodesRack * 2));
+        net.connect(1000, swDstRack1, dstNodes.subList(0, nbNodesRack));
+        net.connect(10000, swMain, swSrcRack1, swSrcRack2, swDstRack1);
+        mo.attach(net);
+        //net.generateDot(path + "topology.dot", false);
+
+        // Set parameters
+        DefaultParameters ps = new DefaultParameters();
+        ps.setVerbosity(0);
+        ps.setTimeLimit(10);
+        //ps.setMaxEnd(600);
+        ps.doOptimize(false);
+
+        // Migrate all VMs to destination nodes
+        List<SatConstraint> cstrs = new ArrayList<>();
+        int vm_num = 0;
+        for (int i = 0; i < nbDstNodes; i++) {
+            cstrs.add(new Fence(vms.get(vm_num), Collections.singleton(dstNodes.get(i))));
+            cstrs.add(new Fence(vms.get(vm_num + 1), Collections.singleton(dstNodes.get(i))));
+            cstrs.add(new Fence(vms.get(nbVMs - 1 - vm_num), Collections.singleton(dstNodes.get(i))));
+            cstrs.add(new Fence(vms.get(nbVMs - 2 - vm_num), Collections.singleton(dstNodes.get(i))));
+            vm_num += 2;
+        }
+
+        // Shutdown source nodes
+        for (Node n : srcNodes) {
+            cstrs.add(new Offline(n));
+        }
+
+        // Set the objective
+        DefaultChocoScheduler sc = new DefaultChocoScheduler(ps);
+        Instance i = new Instance(mo, cstrs,  new MinMTTR());
+
+        ReconfigurationPlan p;
+        try {
+            p = sc.solve(i);
+            Assert.assertNotNull(p);
+
+            ReconfigurationPlanConverter planConverter = new ReconfigurationPlanConverter();
+            JSONObject obj = null;
+            try {
+                obj = planConverter.toJSON(p);
+            } catch (JSONConverterException e) {
+                System.err.println("Error while converting plan: " + e.toString());
+                //e.printStackTrace();
+            }
+            try {
+                FileWriter file = new FileWriter(path + "BtrPlace.json");
+                file.write(obj.toJSONString());
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //energyView.plotConsumption(p, path + "BtrPlace_theory.csv");
+            
+        } finally {
+            System.err.println(sc.getStatistics());
+            System.err.flush();
+        }
+    }
+}
---
+++ src/test/java/org/btrplace/scheduler/ucc15/random/Random.java
@@ -0,0 +1,197 @@
+package org.btrplace.scheduler.ucc15.random;
+
+import net.minidev.json.JSONObject;
+import org.btrplace.json.JSONConverterException;
+import org.btrplace.json.plan.ReconfigurationPlanConverter;
+import org.btrplace.model.*;
+import org.btrplace.model.constraint.Fence;
+import org.btrplace.model.constraint.SatConstraint;
+import org.btrplace.model.constraint.migration.MinMTTRMig;
+import org.btrplace.model.view.ShareableResource;
+import org.btrplace.model.view.network.Network;
+import org.btrplace.model.view.network.Switch;
+import org.btrplace.plan.ReconfigurationPlan;
+import org.btrplace.scheduler.choco.DefaultChocoScheduler;
+import org.btrplace.scheduler.choco.DefaultParameters;
+import org.btrplace.scheduler.choco.transition.MigrateVMTransition;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Created by vkherbac on 19/02/15.
+ */
+public class Random {
+
+    String path = new File("").getAbsolutePath() + "/src/test/java/org/btrplace/scheduler/tests/ucc15/random/";
+
+    public Map<VM,Node> RandomTest(String fileName, Map<VM,Node> placedVMs) {
+
+        // Set nb of nodes and vms
+        int nbNodes = 4;
+        int nbVMs = 10;
+
+        // Set mem + cpu for Nodes and VMs
+        int mem_node = 16, cpu_node = 8;
+        int mem_vm1 = 2, cpu_vm1 = 1;
+        int mem_vm2 = 3, cpu_vm2 = 1;
+
+        // Set memoryUsed and dirtyRate (for all VMs) => "stress --vm 1000 --bytes 70K"
+        int tpl1MemUsed = 2000, tpl1MaxDirtySize = 96, tpl1MaxDirtyDuration = 2; double tpl1DirtyRate = 2;
+        int tpl2MemUsed = 3000, tpl2MaxDirtySize = 96, tpl2MaxDirtyDuration = 2; double tpl2DirtyRate = 2;
+
+        // New default model
+        Model mo = new DefaultModel();
+        Mapping ma = mo.getMapping();
+
+        // Create online nodes
+        List<Node> nodes = new ArrayList<>();
+        for (int i=0; i<nbNodes; i++) { nodes.add(mo.newNode()); ma.addOnlineNode(nodes.get(i)); }
+
+        // Add resource views
+        ShareableResource rcMem = new ShareableResource("mem", 0, 0);
+        ShareableResource rcCPU = new ShareableResource("cpu", 0, 0);
+        for (Node n : nodes) { rcMem.setCapacity(n, mem_node); rcCPU.setCapacity(n, cpu_node); }
+        mo.attach(rcMem); mo.attach(rcCPU);
+
+        // Add attributes
+        for (Node n : nodes) { mo.getAttributes().put(n, "boot", 120); /*~2 minutes to boot*/ }
+        for (Node n : nodes) {  mo.getAttributes().put(n, "shutdown", 30); /*~30 seconds to shutdown*/ }
+
+        // Init VM <-> Mem mapping for random setup
+        Map<VM,Integer> memPerVM = new HashMap<>();
+
+        // Create VMs
+        List<VM> vms = new ArrayList<>();
+        for (int i=0; i<nbVMs/2; i++) {
+            VM v = mo.newVM(); vms.add(v); rcMem.setConsumption(v, mem_vm1); rcCPU.setConsumption(v, cpu_vm1);
+            memPerVM.put(v,mem_vm1);
+            mo.getAttributes().put(vms.get(i), "memUsed", tpl1MemUsed);
+            mo.getAttributes().put(vms.get(i), "dirtyRate", tpl1DirtyRate);
+            mo.getAttributes().put(vms.get(i), "maxDirtySize", tpl1MaxDirtySize);
+            mo.getAttributes().put(vms.get(i), "maxDirtyDuration", tpl1MaxDirtyDuration);
+        }
+        for (int i=nbVMs/2; i<nbVMs; i++) {
+            VM v = mo.newVM(); vms.add(v); rcMem.setConsumption(v, mem_vm2); rcCPU.setConsumption(v, cpu_vm2);
+            memPerVM.put(v,mem_vm2);
+            mo.getAttributes().put(vms.get(i), "memUsed", tpl2MemUsed);
+            mo.getAttributes().put(vms.get(i), "dirtyRate", tpl2DirtyRate);
+            mo.getAttributes().put(vms.get(i), "maxDirtySize", tpl2MaxDirtySize);
+            mo.getAttributes().put(vms.get(i), "maxDirtyDuration", tpl2MaxDirtyDuration);
+
+        }
+
+        if (placedVMs.isEmpty()) {
+            // Setup an initial placement
+            for (VM vm : vms) {
+                ma.addRunningVM(vm, nodes.get(vm.id() % nbNodes));
+                placedVMs.put(vm, nodes.get(vm.id() % nbNodes));
+            }
+        }
+        else {
+            // Setup the input placement
+            for (VM vm : placedVMs.keySet()) { ma.addRunningVM(vms.get(vm.id()), nodes.get(placedVMs.get(vm).id())); }
+        }
+
+        // Init a list of constraints
+        List<SatConstraint> cstrs = new ArrayList<>();
+
+        // Init variables to generate a new random placement
+        java.util.Random r = new java.util.Random(); Node n = null;
+        Map<VM,Node> newPlacedVMs = new HashMap<>();
+        Map<Node,Integer> memPerNode = new HashMap<>();
+        for (int i=0; i<nbNodes; i++) { memPerNode.put(nodes.get(i), 0); }
+
+        // Select the destination node randomly
+        for (VM vm : placedVMs.keySet()) {
+
+            // Select a destination node != source node AND with enough resources
+            do { n = nodes.get(r.nextInt(nodes.size())); }
+            while (memPerVM.get(vm) >= (mem_node - memPerNode.get(n)) || n.equals(placedVMs.get(vm)));
+
+            // Force the migration to the selected destination node
+            cstrs.add(new Fence(vm, Collections.singleton(n)));
+            newPlacedVMs.put(vm, n);
+            memPerNode.replace(n, memPerNode.get(n)+memPerVM.get(vm));
+        }
+
+        // Create a NetworkView view AND connect half of the nodes to a 500Mb/s link
+        Network net = new Network(); Switch swMain = net.newSwitch();
+        //Collections.shuffle(nodes);
+        for (int i=0; i<nbNodes/2; i++) { net.connect(500, swMain, nodes.get(i)); }
+        for (int i=nbNodes/2; i<nbNodes; i++) { net.connect(1000, swMain, nodes.get(i)); }
+        mo.attach(net);
+        //net.generateDot(path + "topology.dot", false);
+
+        // Set parameters
+        DefaultParameters ps = new DefaultParameters();
+        ps.setVerbosity(2);
+        ps.setTimeLimit(10);
+        ps.doOptimize(false);
+
+        // Set the custom migration transition
+        ps.getTransitionFactory().remove(ps.getTransitionFactory().getBuilder(VMState.RUNNING, VMState.RUNNING));
+        ps.getTransitionFactory().add(new MigrateVMTransition.Builder());
+
+        // Set a custom objective
+        DefaultChocoScheduler sc = new DefaultChocoScheduler(ps);
+        Instance i = new Instance(mo, cstrs, new MinMTTRMig());
+
+        ReconfigurationPlan p;
+        try {
+            p = sc.solve(i);
+            Assert.assertNotNull(p);
+
+            ReconfigurationPlanConverter planConverter = new ReconfigurationPlanConverter();
+            JSONObject obj = null;
+
+            try {
+                obj =  planConverter.toJSON(p);
+            } catch (JSONConverterException e) {
+                System.err.println("Error while converting the plan: " + e.toString());
+                e.printStackTrace();
+                newPlacedVMs = null;
+            }
+
+            try {
+                FileWriter file = new FileWriter(path + fileName);
+                file.write(obj.toJSONString());
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                System.err.println("Error while writing the plan: " + e.toString());
+                e.printStackTrace();
+                newPlacedVMs = null;
+            }
+
+            //ActionsToCSV.convert(p.getActions(), path + "actions.csv");
+            System.err.println(p);
+            System.err.flush();
+
+        } catch (Exception e) {
+            newPlacedVMs = null;
+        } finally {
+            //System.err.println(sc.getStatistics());
+            return newPlacedVMs;
+        }
+    }
+
+    @Test
+    public void createRandom() {
+
+        int nb = 50;
+
+        Map<VM,Node> mapVMs = new HashMap<>();
+        Map<VM,Node> newMapVMs = null;
+
+        for (int i=1; i<=nb; i++) {
+            while (newMapVMs == null) { newMapVMs = RandomTest("random." + i + ".json", mapVMs); }
+            mapVMs.clear(); mapVMs.putAll(newMapVMs);
+            newMapVMs.clear(); newMapVMs = null;
+        }
+    }
+}
